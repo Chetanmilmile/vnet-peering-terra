82dd8e3c6e34422555d8771b76a538133895a992
chetan milmile <107764112+Chetanmilmile@users.noreply.github.com>
2023-10-19 13:29:09 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,57 @@
+# Define the Azure provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example-1" {
+  name     = "example-1-resources"
+  location = "East US"
+}
+
+resource "azurerm_resource_group" "example-2" {
+  name     = "example-2-resources"
+  location = "East US"
+}
+
+# Create the hub virtual network
+resource "azurerm_virtual_network" "hub" {
+  name                = "hub-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example-1.location
+  resource_group_name = azurerm_resource_group.example-1.name
+}
+
+# Create the spoke virtual networks
+resource "azurerm_virtual_network" "spoke1" {
+  name                = "spoke-network-1"
+  address_space       = ["10.1.0.0/16"]
+  location            = azurerm_resource_group.example-2.location
+  resource_group_name = azurerm_resource_group.example-2.name
+}
+
+resource "azurerm_virtual_network" "spoke2" {
+  name                = "spoke-network-2"
+  address_space       = ["10.2.0.0/16"]
+  location            = azurerm_resource_group.example-2.location
+  resource_group_name = azurerm_resource_group.example-2.name
+}
+
+# Create virtual network peerings between hub and spokes
+resource "azurerm_virtual_network_peering" "hub_to_spoke1" {
+  name                         = "hub-to-spoke1"
+  resource_group_name          = azurerm_resource_group.example-1.name
+  virtual_network_name         = azurerm_virtual_network.hub.name
+  remote_virtual_network_id    = azurerm_virtual_network.spoke1.id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
+}
+
+resource "azurerm_virtual_network_peering" "hub_to_spoke2" {
+  name                         = "hub-to-spoke2"
+  resource_group_name          = azurerm_resource_group.example-1.name
+  virtual_network_name         = azurerm_virtual_network.hub.name
+  remote_virtual_network_id    = azurerm_virtual_network.spoke2.id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
+}
