8f7ae625cdd84f0d8130126557f1e1c44f1d73e9
chetan milmile <107764112+Chetanmilmile@users.noreply.github.com>
2023-10-19 13:12:56 +0530

    push

---
+++ main.tf
@@ -0,0 +1,52 @@
+# Define the Azure provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}
+
+# Create the hub virtual network
+resource "azurerm_virtual_network" "hub" {
+  name                = "hub-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+# Create the spoke virtual networks
+resource "azurerm_virtual_network" "spoke1" {
+  name                = "spoke-network-1"
+  address_space       = ["10.1.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_virtual_network" "spoke2" {
+  name                = "spoke-network-2"
+  address_space       = ["10.2.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+# Create virtual network peerings between hub and spokes
+resource "azurerm_virtual_network_peering" "hub_to_spoke1" {
+  name                         = "hub-to-spoke1"
+  resource_group_name          = azurerm_resource_group.example.name
+  virtual_network_name         = azurerm_virtual_network.hub.name
+  remote_virtual_network_id    = azurerm_virtual_network.spoke1.id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
+}
+
+resource "azurerm_virtual_network_peering" "hub_to_spoke2" {
+  name                         = "hub-to-spoke2"
+  resource_group_name          = azurerm_resource_group.example.name
+  virtual_network_name         = azurerm_virtual_network.hub.name
+  remote_virtual_network_id    = azurerm_virtual_network.spoke2.id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
+}
